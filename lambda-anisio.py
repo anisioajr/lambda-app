a4879d2a3f4344a46e85fb059feb1e31c84f098e
anisioajr <anisioajr@gmail.com>
2023-11-24 23:08:37 -0300

    Update lambda-anisio.py

--- lambda-anisio.py
+++ lambda-anisio.py
@@ -1,1 +1,11 @@
 # try it
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
+api_secret = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+
+print("API_KEY: ", api_key)
+print("API_SECRET: ", api_secret)
